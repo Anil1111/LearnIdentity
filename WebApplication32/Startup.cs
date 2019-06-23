ff41afba7b9de4841b3dd736a1f892485aceb040
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-06-23 12:10:21 +0530

    Add project files.

---
+++ WebApplication32/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication32.Startup))]
+namespace WebApplication32
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
